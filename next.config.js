c8cf14a285c85265498e555d7d0d4e9db8de5f5d
sunandbean <sunbin.vincent.kim@gmail.com>
2022-04-24 15:28:32 +0900

    redirect, rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = process.env.API_KEY; //"1a0c6929a9132fc7d70882c9e7c5f5aa";
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/contact",
+        destination: "/form",
+        permanent: false,
+      },
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
